26738aeaf8da779607b06a2bcbbb2961eaa1158a
Lab Rat <dev.frolics@gmail.com>
2024-10-02 14:02:37 +0530

    chore: some changes

--- Configs/.config/nvim/lua/custom/configs/lspconfig.lua
+++ Configs/.config/nvim/lua/custom/configs/lspconfig.lua
@@ -1,26 +1,24 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-local on_attach = require("plugins.configs.lspconfig").on_attach
-local capabilities = require("plugins.configs.lspconfig").capabilities
-
 local lspconfig = require "lspconfig"
 local poetry = require "poetry-nvim"
 local com_capabilities = vim.lsp.protocol.make_client_capabilities()
 com_capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 -- Rust
-lspconfig.rust_analyzer.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = {"rust"},
-  root_dir = lspconfig.util.root_pattern("Cargo.toml")
-})
+-- local on_attach = require("plugins.configs.lspconfig").on_attach
+-- local capabilities = require("plugins.configs.lspconfig").capabilities
+-- lspconfig.rust_analyzer.setup({
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+--   filetypes = {"rust"},
+--   root_dir = lspconfig.util.root_pattern("Cargo.toml")
+-- })
 
 -- Go Language
 lspconfig.gopls.setup{}
 
 -- HTML
 lspconfig.html.setup {
-  capabilities = com_capabilities,
 }
 
 -- JAVA
@@ -54,6 +52,7 @@ lspconfig.remark_ls.setup{}
 --     }
 --   }
 -- }
+-- lspconfig.ruff.setup{}
 lspconfig.ruff_lsp.setup{}
 
 poetry.setup()
@@ -69,8 +68,20 @@ lspconfig.typeprof.setup{}
 -- Vue
 lspconfig.vuels.setup{}
 
+-- Bash
+lspconfig.bashls.setup{}
+
+-- Lua
+
+
 -- Dockerfile
 lspconfig.dockerls.setup{}
 
+-- JavaScript
+lspconfig.denols.setup{}
+lspconfig.eslint.setup{}
+lspconfig.flow.setup{}
+
 -- Zig
 lspconfig.zls.setup{}
+
